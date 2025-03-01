ba4f4a326c76f0dbc8df4cbf8096897ae1c5198b
Kallal Mukherjee <ritamukherje62@gmail.com>
2025-03-01 06:02:03 +0530

    Update safe.js

--- safe.js
+++ safe.js
@@ -1,4 +1,4 @@
- import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
  import { getAuth,GoogleAuthProvider,signInWithPopup} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
   
   const firebaseConfig = {
